0e2a7b972ccb6e3fda34ac9867f828319f5225d5
tycheaierdoe <166589230+flyingdeer000000@users.noreply.github.com>
2024-05-13 14:03:58 +0800

    up

--- src/recorder.py
+++ src/recorder.py
@@ -1,4 +1,6 @@
+import os
 import typing
+import wave
 from queue import Queue
 
 import pyaudiowpatch as pyaudio
@@ -6,9 +8,6 @@ import pyaudiowpatch as pyaudio
 
 # import wave
 # import os
-
-# filename = "loopback_record_class.wav"
-# data_format = pyaudio.paInt24
 
 
 class ARException(Exception):
@@ -27,17 +26,14 @@ class AudioRecorder:
     CHUNK_SIZE = 512
 
     def __init__(self,
-                 p_audio: pyaudio.PyAudio,
-                 output_queue: Queue,
-                 channels: int = 1,
+                 p_audio: pyaudio.PyAudio = pyaudio.PyAudio(),
+                 output_queue: Queue = Queue(),
+                 output_channels: int = 1,
                  sample_rate: int = 16000,
                  data_format: int = pyaudio.paInt16,
                  chunk: int = 256,
                  frame_duration: int = 30
                  ):
-
-        if p_audio is None:
-            p_audio = pyaudio.PyAudio()
 
         self.p = p_audio
         self.output_queue = output_queue
@@ -46,31 +42,31 @@ class AudioRecorder:
 
         self.sample_rate = sample_rate
         self.data_format = data_format
-        self.channels = channels
+        self.output_channels = output_channels
         self.chunk = chunk
         self.frame_size = (sample_rate * frame_duration // 1000)
 
         self.__frames: typing.List[bytes] = []
 
-    @staticmethod
-    def get_default_wasapi_device(p_audio: pyaudio.PyAudio):
+    def get_default_wasapi_device(self):
 
         try:  # Get default WASAPI info
-            wasapi_info = p_audio.get_host_api_info_by_type(pyaudio.paWASAPI)
+            wasapi_info = self.p.get_host_api_info_by_type(pyaudio.paWASAPI)
         except OSError:
             raise WASAPINotFound("Looks like WASAPI is not available on the system")
 
         # Get default WASAPI speakers
-        sys_default_speakers = p_audio.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
+        sys_default_speakers = self.p.get_device_info_by_index(wasapi_info["defaultOutputDevice"])
 
         if not sys_default_speakers["isLoopbackDevice"]:
-            for loopback in p_audio.get_loopback_device_info_generator():
+            for loopback in self.p.get_loopback_device_info_generator():
                 if sys_default_speakers["name"] in loopback["name"]:
                     return loopback
                     break
             else:
                 raise InvalidDevice(
-                    "Default loopback output device not found.\n\nRun `python -m pyaudiowpatch` to check available devices")
+                    "Default loopback output device not found.\n\nRun `python -m pyaudiowpatch` to check available "
+                    "devices")
 
     def callback(self, in_data, frame_count, time_info, status):
         """Write frames and return PA flag"""
@@ -97,10 +93,13 @@ class AudioRecorder:
                                   stream_callback=stream_callback
                                   )
 
-        if not split:
-            return self.stream
+        if split:
+            self.splitting()
 
         return self.stream
+
+    def splitting(self):
+        pass
 
     def stop_stream(self):
         self.stream.stop_stream()
@@ -117,3 +116,63 @@ class AudioRecorder:
     @property
     def stream_status(self):
         return "closed" if self.stream is None else "stopped" if self.stream.is_stopped() else "running"
+
+
+if __name__ == 'main':
+    print("start =========== ")
+    p = pyaudio.PyAudio()
+    audio_queue = Queue()
+    ar = AudioRecorder(
+        p_audio=p,
+        output_queue=audio_queue,
+    )
+
+    help_msg = 30 * "-" + ("\n\n\nStatus:\nRunning=%s | Device=%s | output=%s\n\nCommands:\nlist\nrecord {"
+                           "device_index\\default}\npause\ncontinue\nstop {*.wav\\default}\n")
+
+    device_def = ar.get_default_wasapi_device()
+
+    try:
+        while True:
+            print(help_msg % (
+                ar.stream_status, device_def["index"] if device_def is not None else "None"))
+            com = input("Enter command: ").split()
+            if com[0] == "list":
+                p.print_detailed_system_info()
+            elif com[0] == "record":
+                ar.start_recording()
+            elif com[0] == "pause":
+                ar.stop_stream()
+            elif com[0] == "continue":
+                ar.start_stream()
+            elif com[0] == "stop":
+                ar.close_stream()
+
+                filename = "../temp/a.wav"
+
+                if len(com) > 1 and com[1].endswith(".wav") and os.path.exists(
+                        os.path.dirname(os.path.realpath(com[1]))):
+                    filename = com[1]
+
+                wave_file = wave.open(filename, 'wb')
+                wave_file.setnchannels(ar.output_channels)
+                wave_file.setsampwidth(pyaudio.get_sample_size(device_def))
+                wave_file.setframerate(int(device_def["defaultSampleRate"]))
+
+                while not audio_queue.empty():
+                    wave_file.writeframes(audio_queue.get())
+                wave_file.close()
+
+                print(f"The audio is written to a [{filename}]. Exit...")
+                break
+
+            else:
+                print(f"[{com[0]}] is unknown command")
+
+    except KeyboardInterrupt:
+        print("\n\nExit without saving...")
+    finally:
+        ar.close_stream()
+        p.terminate()
+
+    print("end ============= ")
